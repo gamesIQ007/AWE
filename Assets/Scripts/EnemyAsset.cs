fefa06f08f06cfca97d82d244d76396a188d5ff1
gamesIQ007 <bdagames@ya.ru>
2023-10-22 20:25:40 +0300

    Добавлены тайлмапы пола и стен

---
+++ Assets/Scripts/EnemyAsset.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu]
+
+/// <summary>
+/// ScriptableObject со свойствами врагов
+/// </summary>
+public class EnemyAsset : ScriptableObject
+{
+    [Header("Внешний вид")]
+    /// <summary>
+    /// Цвет
+    /// </summary>
+    public Color color = Color.white;
+
+    /// <summary>
+    /// Размер спрайта
+    /// </summary>
+    public Vector2 spriteScale = new Vector2(4, 4);
+
+    /// <summary>
+    /// Анимация
+    /// </summary>
+    //public RuntimeAnimatorController animations;
+        
+    [Header("Игровые параметры")]
+    /// <summary>
+    /// Скорость движения
+    /// </summary>
+    public float moveSpeed = 1.0f;
+
+    /// <summary>
+    /// Здоровье
+    /// </summary>
+    public int hp = 10;
+
+    /// <summary>
+    /// Радиус коллайдера
+    /// </summary>
+    public float radius = 0.4f;
+
+    /// <summary>
+    /// Смещение коллайдера
+    /// </summary>
+    public float colliderOffsetY = 0.0f;
+}
